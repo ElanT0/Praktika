2726845a8a8540421e0419826869373dc26556c9
EvgeniyKron <132811894+ElanT0@users.noreply.github.com>
2023-05-22 14:41:13 +0500

    Delete engine.py

--- engine.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import Session
-
-engine = create_engine("postgresql+psycopg2://postgres:univer@localhost/auf")
-session = Session(bind=engine)
